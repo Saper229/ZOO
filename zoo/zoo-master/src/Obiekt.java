eccace386d57a5aed71607ec6e06962ffc91bf22
Paweł Bernyś <146168580+Saper229@users.noreply.github.com>
2026-03-08 22:59:47 +0100

    Delete zoo/zoo-master directory

--- zoo/zoo-master/src/Obiekt.java
+++
@@ -1,5 +0,0 @@
-public class Obiekt {
-    String nazwa;
-    TypObiektu typ;
-    int pojemnosc;
-}
--- zoo/zoo-master/src/TypObiektu.java
+++
@@ -1,9 +0,0 @@
-public enum TypObiektu {
-    KLATKA,
-    WOLIERA,
-    TERRARIUM,
-    AKWARIUM,
-    WYBIEG
-
-
-}
